5db9edb3af470eddb3968868e717431072e11590
SHAHEED MIRJANAIK <shaheedrm678@gmail.com>
2025-03-11 16:32:04 +0530

    changes made-2

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  //cout << "Hello, from Shaheed\n";
+  cout << "Hello, from Shaheed Mirjanaik\n";
   cout << "Hello, Changes made successfully \n";
   return 0;
 }
